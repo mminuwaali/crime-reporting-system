ff07a804122a82421c14d8c0a49fd2db8331739c
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-08-26 22:51:51 +0100

    Initialized Django project

---
+++ website/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("coreapp.urls")),
+    path("auth/", include("account.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
